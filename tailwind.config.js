02fd0784061effff601679ee20b661d40532524c
omid-develop <omidataee20041382>
2025-09-05 18:10:45 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+   content: ["./src/**/*.{html,js}" , "./index.html"],
+   theme: {
+     extend: {
+        color:{
+            primary:"#FF6070"
+        }
+     },
+   },
+   plugins: [],
+ }
